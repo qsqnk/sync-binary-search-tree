58e134751e48e26da6eed290b2e67f3cc5fc12fe
Alexander Fedkin <st087201@student.spbu.ru>
2021-11-16 05:07:05 +0300

    add compilation flags

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ repositories {
 
 plugins {
     kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31"
 }
 
 sourceSets.test {
@@ -16,10 +15,16 @@ dependencies {
     implementation("org.jetbrains.kotlinx:lincheck:2.14.1")
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    withType<Test> {
+        maxParallelForks = 1
+        jvmArgs("--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+            "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED")
+    }
+    test {
+        useJUnitPlatform()
+    }
 }
